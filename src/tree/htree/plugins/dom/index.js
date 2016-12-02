7d5a25af0bbdf57147c4ceafbc7836d67f2ce213
Nik Coughlin <nrkn.com@gmail.com>
2016-12-02 16:11:20 +1300

    wip moving stuff out
    to make it easier to work with

--- src/tree/htree/plugins/dom/index.js
+++ src/tree/htree/plugins/dom/index.js
@@ -2,13 +2,11 @@
 
 const element = require( './element' )
 const create = require( './create' )
-const clone = require( './clone' )
 const node = require( './node' )
 
 const dom = fn => {
   element( fn )
   create( fn )
-  clone( fn )
   node( fn )
 }
 
